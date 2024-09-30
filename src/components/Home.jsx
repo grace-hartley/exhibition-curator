36e262bc919afbf16039b28305c34c721740e933
grace-hartley <gracerita.hartley@gmail.com>
2024-09-30 10:22:38 +0100

    Update to description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,9 +19,9 @@ const Home = () => {
             to let your personanality shine!
           </p>
           <p>
-            To get started, browze the gallery below or use the search bar to
-            start exporing, where you can futher refine your search. If you want
-            to find out more about a piece of art, simply click on the
+            To get started, browse the gallery below or use the search bar to
+            start exporing, where you can further refine your search. If you
+            want to find out more about a piece of art, simply click on the
             corresponding image. When you find a piece of at that is calling
             your name, add it to your collection by pressing the{" "}
             <span className="inline-flex items-center">
@@ -34,7 +34,7 @@ const Home = () => {
             if you change your mind.
           </p>
           <p>
-            But be careful! When you close the browzer your collection goes with
+            But be careful! When you close the browser your collection goes with
             it - it is there to be enjoyed for a short time, not a long time!
           </p>
         </div>
